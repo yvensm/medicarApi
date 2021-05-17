89628d4e53440191a165a75abd66dfe8b6314157
yvensm <claudioyvens@hotmail.com>
2021-05-17 20:43:22 -0300

    create medicos model

---
+++ medicos/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from especialidades.models import Especialidade
+
+
+class Medico(models.Model):
+    nome = models.CharField(max_length=255)
+    crm = models.IntegerField(unique=True)
+    email = models.EmailField(
+        max_length=100, unique=True, null=True, blank=True)
+    telefone = models.CharField(max_length=13, null=True, unique=True)
+    especialidade = models.ForeignKey(Especialidade, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Médico"
+        verbose_name_plural = "Médicos"
+
+    def __str__:
+        return self.nome
