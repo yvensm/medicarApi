8c4f0cee1ad7acd2d3bb8945385b065684bb268e
yvensm <claudioyvens@hotmail.com>
2021-05-17 20:42:51 -0300

    create especialidades model

---
+++ especialidades/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Especialidade(models.Model):
+    nome = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        verbose_name = "Especialidade"
+        verbose_name_plural = "Especialidades"
+
+    def __str__:
+        return self.nome
