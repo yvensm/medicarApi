4805fff5ffde4a7d3e42dfc7a40e41700daf0532
yvensm <claudioyvens@hotmail.com>
2021-05-17 20:44:14 -0300

    create consulta models

---
+++ consultas/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from medicos.models import Medico
+from users.models import User
+# Create your models here.
+
+
+class Consulta(models.Model):
+    dia = models.DateField()
+    horario = models.TimeField()
+    data_agendamento = models.DateTimeField(auto_now_add=True)
+    medico = models.ForeignKey(Medico, on_delete=models.CASCADE)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Consulta"
+        verbose_name_plural = "Consultas"
