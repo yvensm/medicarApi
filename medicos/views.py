4904a749980b1b1f65e162f2f23fb85ace30585b
yvensm <claudioyvens@hotmail.com>
2021-05-20 17:54:20 -0300

    filtro de medico

--- medicos/views.py
+++ medicos/views.py
@@ -1,3 +1,6 @@
+from functools import reduce
+import operator
+from django.db.models.query_utils import Q
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
 from rest_framework import viewsets
@@ -6,13 +9,32 @@ from .serializers import MedicoSerializer, MedicoListSerializer
 from rest_framework.permissions import IsAuthenticated
 
 
+class MedicoFilter(filters.BaseFilterBackend):
+
+    def filter_queryset(self, request, queryset, view):
+        filtro = {}
+        especialidades = []
+        for param in request.query_params:
+            if(param == 'especialidade'):
+                especialidades = dict(request.GET)['especialidade']
+            if(param == 'search'):
+                filtro['nome__contains'] = request.query_params['search']
+        qset = queryset
+        if(len(especialidades) > 0):
+            list_of_Q = [Q(**{'especialidade': val})
+                         for val in especialidades]
+            qset = qset.filter(reduce(operator.or_, list_of_Q))
+        return qset.filter(**filtro)
+
+
 class MedicoViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
     queryset = Medico.objects.all()
     serializer_class = MedicoSerializer
     filter_backends = [filters.SearchFilter, DjangoFilterBackend]
-    search_fields = ['nome', 'crm']
-    filterset_fields = ['especialidade']
+    # search_fields = ['nome']
+    # filterset_fields = ['especialidade']
+    filter_backends = [MedicoFilter]
 
     def get_serializer_class(self):
         if(self.action == 'list'):
